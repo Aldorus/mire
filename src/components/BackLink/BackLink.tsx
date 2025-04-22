4b5e2077e07aa9318f55de3c69a35acf8d3a69f8
Guillaume R <rousselguillaume@hotmail.fr>
2025-04-21 20:34:17 -0400

    chore: link and animations

--- src/components/BackLink/BackLink.tsx
+++ src/components/BackLink/BackLink.tsx
@@ -2,8 +2,13 @@ import './_BackLink.scss';
 import {ROUTE} from "../../App.tsx";
 import ArrowRight from "../../assets/ArrowRight.svg";
 import {Link} from "../Link/Link.tsx";
+import {HTMLAttributes} from "react";
+import classnames from "classnames";
 
-const BackLink = () => {
-  return <Link to={ROUTE.HOME} className="Backlink"><img className="Backlink__icon" src={ArrowRight}/><span>Back to project</span></Link>
+type Props = {
+  className?: HTMLAttributes<HTMLElement>['className'];
+}
+const BackLink = ({className} : Props) => {
+  return <Link to={ROUTE.HOME} className={classnames(["Backlink", className])}><img className="Backlink__icon" src={ArrowRight}/>&nbsp;<span>Back to project</span></Link>
 }
 export default BackLink;
